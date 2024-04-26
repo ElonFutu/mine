fbb94f306b85c4bce8994dbde594e6b37b5a7d00
like <likeaixi@gmail.com>
2024-04-26 16:43:15 +0800

    add xelis node and shell

---
+++ xelis/xelis-taxminer.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# 检查是否以root用户运行脚本
+if [ "$(id -u)" != "0" ]; then
+    echo "此脚本需要以root用户权限运行。"
+    echo "请尝试使用 'sudo -i' 命令切换到root用户，然后再次运行此脚本。"
+    exit 1
+fi
+
+nohup /root/xelis_taxminer --wallet xel:w2ta8au79jejkcjfqrd08jdm0hhcsuh3a3drrrdc3j5wtvxr7czsqeeateh --host 127.0.0.1:8080 --boot --display-hs-all --worker taxminer >/root/xelis_taxminer-`date +%Y-%m-%d`.log  2>&1 &
+
+echo "[`date '+%Y-%m-%d %H:%M:%s'`] xelis_taxminer started"
